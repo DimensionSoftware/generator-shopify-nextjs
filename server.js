c26c97507d6bbe674c8b4cead822bee3cd7a9542
mr. keith <keith@hoerling.com>
2018-10-19 17:59:44 -0700

    wip: /shopify/* auth routes

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const shopifyAuth = require('@shopify/koa-shopify-auth').default
+
 const Koa = require('koa')
 const next = require('next')
 const Router = require('koa-router')
@@ -7,10 +9,33 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-app.prepare()
+app
+  .prepare()
   .then(() => {
     const server = new Koa()
     const router = new Router()
+      server.use(
+        shopifyAuth({
+          // if specified, mounts the routes off of the given path
+          // eg. /shopify/auth, /shopify/auth/callback
+          // defaults to ''
+          prefix: '/shopify',
+          // your shopify app api key
+          apiKey: process.env.SHOPIFY_API_KEY,
+          // your shopify app secret
+          secret: process.env.SHOPIFY_SECRET,
+          // scopes to request on the merchants store
+          scopes: ['write_orders, write_products'],
+          // callback for when auth is completed
+          afterAuth(ctx) {
+            const {shop, accessToken} = ctx.session;
+
+            console.log('We did it!', accessToken);
+
+            ctx.redirect('/');
+          },
+        })
+      )
 
     router.get('/a', async ctx => {
       await app.render(ctx.req, ctx.res, '/b', ctx.query)
