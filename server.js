e834365b57451bb191d5637d20530c73a2de4a3e
John BEPPU <john.beppu@gmail.com>
2018-10-19 18:52:57 -0700

    Add koa-session and use it

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const shopifyAuth = require('@shopify/koa-shopify-auth').default
 const Koa = require('koa')
 const next = require('next')
 const Router = require('koa-router')
+const session = require('koa-session')
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
@@ -16,28 +17,29 @@ app
   .then(() => {
     const server = new Koa()
     const router = new Router()
-      server.use(
-        shopifyAuth({
-          // if specified, mounts the routes off of the given path
-          // eg. /shopify/auth, /shopify/auth/callback
-          // defaults to ''
-          prefix: '/shopify',
-          // your shopify app api key
-          apiKey: process.env.SHOPIFY_API_KEY,
-          // your shopify app secret
-          secret: process.env.SHOPIFY_SECRET,
-          // scopes to request on the merchants store
-          scopes: ['write_orders, write_products'],
-          // callback for when auth is completed
-          afterAuth(ctx) {
-            const {shop, accessToken} = ctx.session;
+    server
+      .use(session(server))
+      .use(shopifyAuth({
+        // if specified, mounts the routes off of the given path
+        // eg. /shopify/auth, /shopify/auth/callback
+        // defaults to ''
+        prefix: '/shopify',
+        // your shopify app api key
+        apiKey: process.env.SHOPIFY_API_KEY,
+        // your shopify app secret
+        secret: process.env.SHOPIFY_SECRET,
+        // scopes to request on the merchants store
+        scopes: ['write_orders, write_products'],
+        // callback for when auth is completed
+        afterAuth(ctx) {
+          const {shop, accessToken} = ctx.session;
 
-            console.log('We did it!', accessToken);
+          console.log('We did it!', accessToken);
 
-            ctx.redirect('/');
-          },
-        })
-      )
+          ctx.redirect('/');
+        },
+      })
+    )
 
     router.get('/a', async ctx => {
       await app.render(ctx.req, ctx.res, '/b', ctx.query)
